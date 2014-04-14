13b3ed52e9133fb3ca04444b351152959a415cdc
Shigenobu Nishikawa <shigenobu.nishikawa@generace.co.jp>
2014-04-14 15:22:59 +0900

    remove game resource cassettes

--- lib/riot_games_api.rb
+++ lib/riot_games_api.rb
@@ -3,9 +3,12 @@ require_relative 'riot_games_api/lol/client'
 require_relative 'riot_games_api/lol/resource'
 require_relative 'riot_games_api/lol/model'
 
+require 'bundler/setup'
+
 require 'json'
 require 'faraday'
 require 'faraday_middleware'
+
 
 module RiotGamesApi
 end
