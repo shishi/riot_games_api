13f5de8bb5291364af73167423d108f729b8987a
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2014-04-20 03:07:32 +0900

    add champion ids and item ids to constats

--- lib/riot_games_api.rb
+++ lib/riot_games_api.rb
@@ -3,11 +3,13 @@ require 'riot_games_api/lol/client'
 require 'riot_games_api/lol/resource'
 require 'riot_games_api/lol/model'
 
+require 'riot_games_api/lol/constant/champions'
+require 'riot_games_api/lol/constant/items'
+
 require 'json'
 require 'faraday'
 require 'faraday_middleware'
 require 'virtus'
 
-# TODO RiotGamesApi::LOL::Model may be just Dto
 module RiotGamesApi
 end
