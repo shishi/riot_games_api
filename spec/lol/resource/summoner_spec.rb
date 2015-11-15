01bdd68e48e25c41fcab6da5870935a5080f6699
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2015-11-15 23:34:21 +0900

    update summoner specs

--- spec/lol/resource/summoner_spec.rb
+++ spec/lol/resource/summoner_spec.rb
@@ -2,79 +2,79 @@ require 'spec_helper'
 
 describe RiotGamesApi::LOL::Resource::Summoner, :vcr do
   let(:client) { RiotGamesApi::LOL::Client.new }
-  let(:summoner_id) { 42139310 }
-  let(:summoner_name) { 'shishisn' }
+  let(:summoner_id) { 64878685 }
+  let(:summoner_name) { 'hello im shishi' }
   let(:summoner_level) { 30 }
 
   describe '#by_name' do
-    let(:summoner) { client.summoner.by_name summoner_name }
+    let(:summoner) { client.summoner.by_name [summoner_name] }
 
     it 'should have key which is summoner name' do
-      summoner.keys.first.should eq summoner_name
+      summoner.first.name.should eq summoner_name
     end
 
     it 'should have summoner model' do
-      summoner.values.first.class.should eq RiotGamesApi::LOL::Model::Summoner::Summoner
+      summoner.first.class.should eq RiotGamesApi::LOL::Model::Summoner::Summoner
     end
 
     it 'should summoner model have summoner level' do
-      summoner.values.first.summoner_level.should eq summoner_level
+      summoner.first.summoner_level.should eq summoner_level
     end
   end
 
   describe '#by_id' do
-    let(:summoner) { client.summoner.by_id summoner_id }
+    let(:summoner) { client.summoner.by_id [summoner_id] }
 
     it 'should have key which is summoner id' do
-      summoner.keys.first.should eq summoner_id
+      summoner.first.id.should eq summoner_id
     end
 
     it 'should have summoner model' do
-      summoner.values.first.class.should eq RiotGamesApi::LOL::Model::Summoner::Summoner
+      summoner.first.class.should eq RiotGamesApi::LOL::Model::Summoner::Summoner
     end
 
     it 'should summoner model have summoner level' do
-      summoner.values.first.summoner_level.should eq summoner_level
+      summoner.first.summoner_level.should eq summoner_level
     end
   end
 
   describe '#masteries' do
-    let(:masteries) { client.summoner.masteries summoner_id }
+    let(:masteries) { client.summoner.masteries [summoner_id] }
 
     it 'should have key which is summoner id' do
-      masteries.keys.first.should eq summoner_id
+      masteries.first.summoner_id.should eq summoner_id
     end
 
     it 'should have mastery page list model' do
-      masteries.values.first.class.should eq RiotGamesApi::LOL::Model::Summoner::MasteryPageList
+      masteries.first.class.should eq RiotGamesApi::LOL::Model::Summoner::MasteryPageList
     end
 
     it 'should have mastery page model' do
-      masteries.values.first.pages.first.class.should eq RiotGamesApi::LOL::Model::Summoner::MasteryPage
+      masteries.first.pages.first.class.should eq RiotGamesApi::LOL::Model::Summoner::MasteryPage
     end
   end
 
   describe '#name' do
-    let(:name) { client.summoner.name summoner_id }
+    let(:name) { client.summoner.name [summoner_id] }
 
     it 'should have summoner name' do
-      name.values.first.should eq summoner_name
+      name.first.name.should eq summoner_name
     end
   end
 
   describe '#runes' do
-    let(:runes) { client.summoner.runes summoner_id }
+    let(:runes) { client.summoner.runes [summoner_id] }
 
     it 'should have key which is summoner id' do
-      runes.keys.first.should eq summoner_id
+      runes.first.summoner_id.should eq summoner_id
     end
 
     it 'should have rune  page list model' do
-      runes.values.first.class.should eq RiotGamesApi::LOL::Model::Summoner::RunePageList
+      runes.first.class.should eq RiotGamesApi::LOL::Model::Summoner::RunePageList
     end
 
     it 'should have rune page model' do
-      runes.values.first.pages.first.class.should eq RiotGamesApi::LOL::Model::Summoner::RunePage
+      runes.first.pages.first.class.should eq RiotGamesApi::LOL::Model::Summoner::RunePage
     end
   end
 end
