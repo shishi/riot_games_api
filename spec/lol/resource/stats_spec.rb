67eea90b02ec7c34f52b77bc663d6673160edba8
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2015-11-16 05:58:02 +0900

    add documents and fix specs for stats

--- spec/lol/resource/stats_spec.rb
+++ spec/lol/resource/stats_spec.rb
@@ -2,35 +2,32 @@ require 'spec_helper'
 
 describe RiotGamesApi::LOL::Resource::Stats, :vcr do
   let(:client) { RiotGamesApi::LOL::Client.new }
-  let(:summoner_id) { 42139310 }
-  let(:season3) { 'SEASON3' }
+  let(:summoner_id) { 64878685 }
+  let(:season4) { 'SEASON2014' }
+  let(:season5) { 'SEASON2015' }
 
   describe '#ranked' do
-    context 'season3' do
-      let(:ranked_stats) { client.stats.ranked summoner_id, season3 }
-      let(:last_modified) { Time.at(1378635320) }
+    context 'season4' do
+      let(:ranked_stats) { client.stats.ranked summoner_id, season4 }
 
-      it 'should have modify_data which unixtime milliseconds' do
-        ranked_stats.modify_date.should eq last_modified
-      end
 
-      it 'should have champion stats' do
-        ranked_stats.champions.first.class.should eq RiotGamesApi::LOL::Model::Stats::ChampionStats
+      it 'should not found ranked stats' do
+        expect{ ranked_stats }.to raise_error Faraday::Error::ResourceNotFound
       end
     end
 
     context 'latest season' do
       let(:ranked_stats) { client.stats.ranked summoner_id }
 
-      it 'should not found ranked stats' do
-        expect{ ranked_stats }.to raise_error Faraday::Error::ResourceNotFound
+      it 'should have champion stats' do
+        ranked_stats.champions.first.class.should eq RiotGamesApi::LOL::Model::Stats::ChampionStats
       end
     end
   end
 
   describe '#summary' do
-    context 'season3' do
-      let(:summary_stats) { client.stats.summary summoner_id, season3 }
+    context 'season5' do
+      let(:summary_stats) { client.stats.summary summoner_id, season5 }
 
       it 'should have summoner id inputed' do
         summary_stats.summoner_id.should eq summoner_id
@@ -42,7 +39,7 @@ describe RiotGamesApi::LOL::Resource::Stats, :vcr do
     end
 
     context 'latest season' do
-      let(:summary_stats) { client.stats.summary summoner_id, season3 }
+      let(:summary_stats) { client.stats.summary summoner_id }
 
       it 'should have summoner id inputed' do
         summary_stats.summoner_id.should eq summoner_id
